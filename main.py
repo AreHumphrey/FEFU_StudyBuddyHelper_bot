71c783cb150b720885d20c3c4e6d4fd4bfb6e79d
Dina <lutdina04@yandex.ru>
2024-04-28 22:18:12 +1000

    fix request, add new button

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import os
 import logging
+from email import message
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
@@ -57,7 +59,8 @@ ACTIONS_KEYBOARD = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text="Подать заявку"),
-            KeyboardButton(text="Изменить свою дисциплину")
+            KeyboardButton(text="Изменить свою дисциплину"),
+            KeyboardButton(text="Посмотреть заявки")
         ]
     ],
     resize_keyboard=True,
@@ -72,11 +75,32 @@ async def cmd_start(message: types.Message):
         parse_mode=ParseMode.HTML,
         reply_markup=SUBSCRIBED_DISCIPLINES_KEYBOARD)
 
+
+@dp.message(lambda message: message.text == "Посмотреть заявки")
+async def handle_view_requests(message: types.Message):
+    user_id = message.from_user.id
+    user = User.get_or_none(user_id=user_id)
+    if user:
+        active_requests = HelpRequest.select().where(HelpRequest.discipline == user.subscribed_disciplines)
+
+        for request in active_requests:
+            author_contacts = f"Контакты автора заявки:\n"
+            user = User.get_or_none(user_id=request.user_id)
+            if user:
+                if user.username:
+                    author_contacts += f"Username: @{user.username}\n"
+
+            await bot.send_message(request.user_id, f"Появилась новая заявка по вашей выбранной дисциплине!\n\n"
+                                                    f"Текст заявки:\n\n {request.request_link}\n\n"
+                                                    f"{author_contacts}", reply_markup=ACTIONS_KEYBOARD)
+
 @dp.message(lambda message: message.text == "Подать заявку")
 async def request_help(message: types.Message):
     user_id = message.from_user.id
 
-    await message.reply("Вы хотите подать заявку? Выберите дисциплину по которой будет ваша заявка:\n" + SUBSCRIPHED_DISCIPLINES_MESSAGE)
+    await message.reply(
+        "Вы хотите подать заявку? Выберите дисциплину по которой будет ваша заявка:\n" + SUBSCRIPHED_DISCIPLINES_MESSAGE)
+
 
 @dp.message(lambda message: message.text.startswith("№"))
 async def choose_discipline_for_request(message: types.Message):
@@ -92,25 +116,12 @@ async def choose_discipline_for_request(message: types.Message):
     discipline = DISCIPLINES[discipline_number - 1]
     await message.reply("Напишите текст заявки:")
 
-    # Save the chosen discipline for the user
     user = User.get_or_none(user_id=user_id)
     if user:
         user.subscribed_disciplines = discipline
         user.save()
         db.commit()
 
-@dp.message(lambda message: message.text)
-async def save_request_text(message: types.Message):
-    user_id = message.from_user.id
-    request_text = message.text
-
-    await message.reply("Введите свой ник в телеграмме:")
-
-    user = User.get_or_none(user_id=user_id)
-    if user:
-        user_request = HelpRequest(user_id=user_id, discipline=user.subscribed_disciplines, request_link=request_text, anonymous=False)
-        user_request.save()
-        db.commit()
 
 @dp.message(lambda message: message.text.startswith("Поменяй на №"))
 async def change_disciplines(message: types.Message):
@@ -122,7 +133,8 @@ async def change_disciplines(message: types.Message):
         if discipline_number < 1 or discipline_number > len(DISCIPLINES):
             raise ValueError
     except ValueError:
-        await message.reply("Неверный формат ввода. Пожалуйста, укажите номер дисциплины из списка.", reply_markup=ACTIONS_KEYBOARD)
+        await message.reply("Неверный формат ввода. Пожалуйста, укажите номер дисциплины из списка.",
+                            reply_markup=ACTIONS_KEYBOARD)
         return
 
     new_discipline = DISCIPLINES[discipline_number - 1]
@@ -131,9 +143,11 @@ async def change_disciplines(message: types.Message):
         user.subscribed_disciplines = new_discipline
         user.save()
         db.commit()
-        await message.reply(f"Дисциплина поменялась на:\n <b>{new_discipline}</b>", reply_markup=ACTIONS_KEYBOARD)
+        await message.reply(f"Дисциплина поменялась на:\n<b>{new_discipline}</b>", reply_markup=ACTIONS_KEYBOARD,
+                            parse_mode=ParseMode.HTML, )
     else:
         await message.reply("Произошла ошибка. Пожалуйста, попробуйте еще раз.", reply_markup=ACTIONS_KEYBOARD)
+
 
 @dp.message(lambda message: message.text == "Выбрать свою дисциплину" or any(char.isdigit() for char in message.text))
 async def show_disciplines_list(message: types.Message):
@@ -159,7 +173,7 @@ async def show_disciplines_list(message: types.Message):
         db.commit()
         formatted_disciplines = ", ".join(disciplines)
         await message.reply(
-            f"Выбрана дисциплина:\n<b>{formatted_disciplines}</b>.\nТеперь ты сможешь помогать другим студентам тоже в ней разобраться\nКоманды которыми ты можешь воспользоваться:\n<b>Подать заявку</b> - ты сможешь попросить помощи у студентов по необходимой дисциплине",
+            f"Выбрана дисциплина:\n<b>{formatted_disciplines}</b>.\nТеперь ты сможешь помогать другим студентам тоже в ней разобраться\nКоманды которыми ты можешь воспользоваться:\n<b>Подать заявку</b> - ты сможешь попросить помощи у студентов по необходимой дисциплине \n <b>Изменить свою дисциплину</b> - ты можешь поменять дисциплину в которой разбираешься, воспользовавшись этой командой",
             parse_mode=ParseMode.HTML,
             reply_markup=ACTIONS_KEYBOARD
         )
@@ -170,58 +184,26 @@ async def show_disciplines_list(message: types.Message):
     await message.reply(CHANGE_DISCIPLINES_MESSAGE, reply_markup=ACTIONS_KEYBOARD)
 
 
-
-
-async def send_request_notifications(user_id: int, subscribed_discipline: str):
-    # Retrieve help requests for the subscribed discipline
-    help_requests = HelpRequest.select().where(HelpRequest.discipline == subscribed_discipline)
-
-    if help_requests:
-        # Iterate over each help request and send a notification message to the user
-        for request in help_requests:
-            # Format the notification message with the help request text and the telegram username
-            notification_text = f"Заявка по вашей дисциплине:\n\n{request.request_link} / {request.user_id}"
-
-            # Send the notification message to the user
-            await bot.send_message(user_id, notification_text)
-    else:
-        # If no help requests found for the subscribed discipline, notify the user
-        await bot.send_message(user_id, "Пока нет заявок по вашей дисциплине.")
 @dp.message(lambda message: message.text)
-async def save_request_text(message: types.Message):
+async def save_request_and_nickname(message: types.Message):
     user_id = message.from_user.id
-    request_text = message.text
-
-    await message.reply("Введите свой ник в телеграмме:")
-
-    # Save the request text for the user
     user = User.get_or_none(user_id=user_id)
-    if user:
-        user_request = HelpRequest(user_id=user_id, discipline=user.subscribed_disciplines, request_link=request_text, anonymous=False)
+    if not user:
+        await message.reply("Произошла ошибка. Пожалуйста, попробуйте еще раз.")
+        return
+
+    user_request = HelpRequest.get_or_none(user_id=user_id, discipline=user.subscribed_disciplines)
+
+    if not user_request:
+        user_request = HelpRequest(user_id=user_id, discipline=user.subscribed_disciplines,
+                                   request_link=message.text,
+                                   anonymous=True)
         user_request.save()
         db.commit()
+        await message.reply(f"Спасибо за вашу заявку!\n\nТекст заявки: {user_request.request_link}\n\nОжидайте ответа.",
+                            reply_markup=ACTIONS_KEYBOARD)
 
-        # Send request notifications to users subscribed to the same discipline
-        await send_request_notifications(user_id, user.subscribed_disciplines)  # Add this line
-@dp.message(lambda message: message.text)
-async def save_request_text(message: types.Message):
-    user_id = message.from_user.id
-    request_text = message.text
 
-    await message.reply("Введите свой ник в телеграмме:")
-
-    # Save the request text for the user
-    user = User.get_or_none(user_id=user_id)
-    if user:
-        user_request = HelpRequest(user_id=user_id, discipline=user.subscribed_disciplines, request_link=request_text, anonymous=False)
-        user_request.save()
-        db.commit()
-
-        # Send request notifications to users subscribed to the same discipline
-        await send_request_notifications(user_id, user.subscribed_disciplines)
-
-        # Send a reply to the user
-        await message.reply("Спасибо за вашу заявку! Ожидайте ответа.")
 
 
 async def main():
--- models.py
+++ models.py
@@ -2,20 +2,25 @@ from peewee import *
 
 db = SqliteDatabase('identifier.sqlite')
 
+
 class BaseModel(Model):
     class Meta:
         database = db
+
 
 class User(BaseModel):
     user_id = IntegerField(unique=True)
     username = CharField(null=True)
     subscribed_disciplines = CharField()
 
+
 class HelpRequest(BaseModel):
     user_id = IntegerField()
     discipline = CharField()
     request_link = CharField()
     anonymous = BooleanField()
+    teg_tg = CharField()
+
 
 db.connect()
 db.create_tables([User, HelpRequest])
